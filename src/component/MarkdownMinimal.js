941bba28ce1d50d6e7c63e41c005b478e3fcfca9
facobackup <gustavomicaelbarbosa@gmail.com>
2022-05-10 09:29:58 -0300

    NPM package

--- src/component/MarkdownMinimal.js
+++ src/component/MarkdownMinimal.js
@@ -1,11 +1,8 @@
-import React, {useEffect, useMemo, useRef, useState} from 'react'
-
+import React from 'react'
 import PropTypes from "prop-types";
 import styles from '../styles/Markdown.module.css'
-import Navigation from "./Navigation";
 
 export default function MarkdownMinimal(props) {
-    const ref = useRef()
     return (
         <article className={styles.article} dangerouslySetInnerHTML={{__html: props.hook.data}}/>
     )
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import Markdown from './component/Markdown'
 import useMarkdown from './hooks/useMarkdown'
 import MarkdownMinimal from './component/MarkdownMinimal'
-export {Markdown, useMarkdown}
+export {Markdown, useMarkdown, MarkdownMinimal}
